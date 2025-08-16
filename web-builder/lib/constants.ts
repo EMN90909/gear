715b74e9c0b0859b69729d5afeca724faf985e60
EMN90909 <nasongoemmanuel8@gmail.com>
2025-08-16 21:43:25 +0300

    Update constants.ts

--- web-builder/lib/constants.ts
+++ web-builder/lib/constants.ts
@@ -1,1 +1,30 @@
+import { 
+  Heading1, 
+  Text, 
+  Image, 
+  PanelTop, 
+  PanelBottom 
+} from 'lucide-react';
 
+export const COMPONENT_TYPES = {
+  heading: {
+    text: "Heading",
+    icon: Heading1
+  },
+  paragraph: {
+    text: "Paragraph",
+    icon: Text
+  },
+  image: {
+    text: "Image",
+    icon: Image
+  },
+  header: {
+    text: "Header",
+    icon: PanelTop
+  },
+  footer: {
+    text: "Footer",
+    icon: PanelBottom
+  }
+};
