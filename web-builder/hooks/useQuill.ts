2dda60ccdc11cc636e86e9d2b10c062909bdf1c0
EMN90909 <nasongoemmanuel8@gmail.com>
2025-08-16 21:44:08 +0300

    Update useQuill.ts

--- web-builder/hooks/useQuill.ts
+++ web-builder/hooks/useQuill.ts
@@ -1,1 +1,19 @@
+import { useEffect, useState } from 'react';
 
+export default function useQuill() {
+  const [quill, setQuill] = useState<any>(null);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.quilljs.com/1.3.6/quill.js';
+    script.onload = () => setLoaded(true);
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return { quill, loaded };
+}
